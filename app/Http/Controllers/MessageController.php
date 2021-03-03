234bdcd6c39678c7f3f6f06a1985bf25e0674623
pc-dorvi <darius.orvidas@presentconnection.eu>
2021-03-03 14:39:57 +0200

    Clean controller

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,25 +2,32 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Vonage\Client;
+use Vonage\SMS\Message\SMS;
 
 class MessageController extends Controller
 {
-    public function __construct()
+    /**
+     * @var Client
+     */
+    private $client;
+
+    public function __construct(Client $client)
     {
         $this->middleware(['auth']);
+
+        $this->client = $client;
     }
     public function index()
     {
         return view('messages');
     }
 
-    public function sendMessages(Request $request, Client $client)
+    public function sendMessages()
     {
         $number = '+37060552136';
-        $response = $client->sms()->send(
-            new \Vonage\SMS\Message\SMS($number, 'Company', 'A text message sent using the Nexmo SMS API')
+        $response = $this->client->sms()->send(
+            new SMS($number, 'Company', 'A text message sent using the Nexmo SMS API')
         );
 
         $message = $response->current();
